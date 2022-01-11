966564cd1fd6dd17237ad4a2511bda9cfe821ab6
streamsnipersgames <streamsnipersgames@gmail.com>
2022-01-11 18:00:07 -0500

    move db connection into each function, change gsk table, add new get api endpoint

--- app.py
+++ app.py
@@ -1,42 +1,55 @@
 from flask import Flask, redirect, render_template, request
-import psycopg2
-import datetime
+import psycopg2, json
+from datetime import datetime
 from decouple import config
-print(config)
-# not preferred method to use pycog directly. good enough for first step
-# preferred method is SQLAlchemy
-con = psycopg2.connect(
-    user=config('DB_USER'),
-    password=config('PASSWORD'),
-    host=config('HOST'),
-    database=config('DB_NAME'),
-    port=config('PORT')
-)
 
 app = Flask(__name__)
 
 
+def establish_db_connection():
+    # not preferred method to use pycog directly. good enough for first step
+    # preferred method is SQLAlchemy
+    conn = psycopg2.connect(
+        user=config('DB_USER'),
+        password=config('PASSWORD'),
+        host=config('HOST'),
+        database=config('DB_NAME'),
+        port=config('PORT')
+    )
+    return conn
+
+
 @app.route('/api/url-reader/gamescorekeeper/', methods=('GET', 'POST'))
 def gameScoreKeeper():
+    con = establish_db_connection()
     status = None
     if request.method == 'POST':
         # get parameter from the form
         twitch_url = request.form['twitch_url']
-        # get cursor
-        cur = con.cursor()
-        # to avoid sql injections parametrised/compiled query
-        cur.execute("INSERT INTO common_site_valorant_twitch_url (twitch_url, added_on_to_db) VALUES (%s, %s)", (twitch_url, datetime.datetime.now()))
-        # commit transactions
-        con.commit()
-        # close curson and connection
-        cur.close()
-        cur.close()
-
-        # TODO: add error check
+        with con.cursor() as cur:
+            # to avoid sql injections parametrised/compiled query
+            cur.execute(
+                f"INSERT INTO api_gsk_fifa (twitch_url, added_on_to_db) VALUES ('{twitch_url}', '{int(datetime.now().timestamp())}')",
+            )
+            # commit transactions
+            con.commit()
         status = True
-        return render_template('twitch_url.html', status=status)
+    con.close()
+    return render_template('twitch_url.html', status=status)
+
+
+@app.route('/api/duel/supported_games/', methods=('GET',))
+def get_duel_supported_games():
+    con = establish_db_connection()
+    status = None
+    if request.method == "GET":
+        with con.cursor() as cur:
+            cur.execute("select game_id, game_name from api_duel_supportedgames")
+        results = json.dumps({x[0]: x[1] for x in cur.fetchall()})
     else:
-        return render_template('twitch_url.html', status=status)
+        results = json.dumps({})
+    con.close()
+    return results
 
 
 @app.route('/', defaults={'path': ''})
